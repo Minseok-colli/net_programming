56569235aa0a627db3f759eb66bd8083993f7d85
Minseok_colli <Minseok-colli@github.com>
2022-03-24 20:37:14 +0900

    hw2

---
+++ HW2/hw2_client.py
@@ -0,0 +1,16 @@
+from http import client
+import socket
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+addr = ('localhost',9000)
+
+sock.connect(addr)
+msg = sock.recv(1024)
+print(msg.decode())
+
+sock.send(b'Park Minseok')
+
+msg = sock.recv(1024)
+print(int.from_bytes(msg,'big'))
+
+sock.close()
