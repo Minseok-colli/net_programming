d0aa111ebf951acdc508dc15bbb1f40dec7751e3
Minseok_colli <Minseok-colli@github.com>
2022-04-10 19:59:49 +0900

    Study_midterm

--- Practice/note.py
+++ Practice/note.py
@@ -164,3 +164,292 @@
 #     cal.send(str(send).encode())
         
 
+# import sys
+# import socket
+
+# length = 4
+# sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# addr = ('localhost',9999)
+# sock.connect(addr)
+
+# sock.send(b'Hello')
+
+# msg = sock.recv(1024)
+
+# if not msg:
+#     sock.close()
+#     sys.exit()
+
+# elif msg != b'Filename':
+#     sock.close()
+#     sys.exit()
+
+# else:
+#     print('server : ',msg.decode())
+
+# filename = input("Enter the file name")
+# sock.send(filename.encode())
+
+# msg = sock.recv(1024)
+# if not msg:
+#     sock.close()
+#     sys.exit()
+
+# elif msg == b'noFile':
+#     sock.close()
+#     sys.exit()
+
+# else:
+#     rx_size = len(msg)
+#     data = msg
+#     while rx_size < length:
+#         msg = sock.recv(1024)
+#         if not msg:
+#             sock.close()
+#             sys.exit()
+#         data = data + msg
+#         rx_size += len(msg)
+#     if rx_size<length:
+#         sock.close()
+#         sys.exit()
+
+#     filesize = int.from_bytes(data,'big')
+#     print('server : ',filesize)
+
+# rx_size = 0
+# f = open(filename,'wb')
+# while rx_size < filesize:
+#     data = sock.recv(1024)
+#     if not data:
+#         break
+#     f.write(data)
+#     rx_size += len(data)
+
+# if rx_size<filesize:
+#     sock.close()
+#     sys.exit()
+
+# print('download complete')
+# sock.send(b'bye')
+# f.close()
+# sock.close()
+
+
+# import sys
+# import socket
+
+# s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# addr = ('localhost',9999)
+# s.connect(addr)
+# print('connected')
+
+# s.send(b'Hello')
+
+# #recv 'filename'
+# msg = s.recv(1024)
+# if not msg:
+#     s.close()
+#     sys.exit()
+# elif msg != b'Filename':
+#     s.close()
+#     sys.exit()
+# else:
+#     print(msg.decode())
+#     filename = input('Enter the file name:')
+
+# s.send(filename.encode())
+
+# #filesize
+
+# msg = s.recv(1024)
+# if not msg:
+#     s.close()
+#     sys.exit()
+# elif msg == b'nofile':
+#     s.close()
+#     sys.exit()
+
+# filesize = int(msg.decode())
+# print('filesize: ',filesize)
+
+# f = open(filename,'wb')
+# rx_size = 0
+
+# while rx_size < filesize:
+#     data = s.recv(1024)
+#     if not data:
+#         s.close()
+#         sys.exit()
+#     f.write(data)
+#     rx_size += len(data)
+
+# if rx_size < filesize:
+#     s.close()
+#     sys.exit()
+
+# print('Download complete')
+# s.send(b'bye')
+
+# s.close()
+# sys.exit()
+
+# import socket
+# import random
+
+# s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# addr = ('localhost',9999)
+
+# s.connect(addr)
+# print('connect completed')
+
+# msg = s.recv(1024)
+# if not msg:
+#     s.close()
+# elif msg != b'Request':
+#     s.close()
+# else:
+#     print('Server: ',msg.decode())
+
+# temp = random.randint(0,40)
+# humid = random.randint(0,100)
+# light = random.randint(70,150)
+
+# data = str(temp) +' '+ str(humid) +' '+ str(light)
+# print(type(data))
+# print(data)
+
+# s.send(data.encode())
+
+# import socket
+
+# s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+# s.bind(('',9999))
+
+# while True:
+#     msg, addr = s.recvfrom(1024)
+#     print('Received: ',msg.decode())
+
+#     s.sendto(msg,addr)
+
+# import socket
+
+# s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+# s.bind(('',3000))
+
+# while True:
+#     data, addr = s.recvfrom(1024)
+#     print("received: ",data.decode())
+#     if data.decode() == 'q':
+#         s.close()
+#     send = input('Send: ')
+#     s.sendto(send.encode(),addr)
+
+
+# import socket
+# import random
+# s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+# s.bind(('',4000))
+
+# while True:
+#     data, addr = s.recvfrom(1024)
+#     if random.randint(1,10) <= 8:
+#         print("packet from {} is lost!".format(addr))
+#         continue
+#     print('packet is {} from {}'.format(data.decode(),addr))
+#     s.sendto('ack'.encode(),addr)
+
+
+#server
+
+# import socket
+# from collections import deque
+
+# s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+# s.bind(('',3000))
+# box = {}
+# while True:
+#     rec, addr = s.recvfrom(1024)
+#     rec = rec.decode()
+    
+#     if rec.startswith('send'):
+#         temp = list(rec.split())
+
+#         id = temp[1]
+#         msg = temp[2:]
+#         mesg = ''
+        
+#         for i in msg:
+#             mesg = str(mesg) + str(i) + ' '
+
+#         if id not in box:
+#             box[str(id)] = deque()
+#             box[str(id)].append(mesg)
+
+#         elif id in box:
+#             box[str(id)].append(mesg)
+        
+#     elif rec.startswith('receive'):
+#         recv_box = temp.split()[2]
+        
+#         if recv_box in box:
+#             if box[str(recv_box)]:
+#                 sndMsg = box[str(recv_box)].popleft()
+#                 s.sendto(sndMsg.encode(),addr)
+#             else:
+#                 s.sendto("no messages".encode(),addr)
+        
+#         else:
+#             s.sendto("no messages".encode(),addr)
+
+#     elif rec.startswith('quit'):
+#         s.close()
+
+
+import socket
+from collections import deque
+
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s.bind(('',3000))
+
+mboxID = {}
+while True:
+    data, addr = s.recvfrom(1024)
+    temp = data.decode().split()
+
+    # print ('temp:', temp)
+
+    command = temp[0]
+    boxid = temp[1]
+    msg_tmp = temp[2:]
+    msg = ''
+    # print ('msg_tmp:', msg_tmp)
+    for i in msg_tmp:
+        msg = msg + str(i) + ' '
+    # print('msg:', msg)
+
+    print('command: {}, boxid: {}, msg: {}'.format(command,boxid,msg))
+
+    if command == 'send':
+        if boxid not in mboxID:
+            mboxID[str(boxid)] = deque()
+            mboxID[str(boxid)].append(msg)
+        
+        else:
+            mboxID[str(boxid)].append(msg)
+
+        # print(msg,'is stored at ',mboxID[str(boxid)])
+
+    elif command == 'receive':
+        if boxid not in mboxID:
+            s.sendto(b'No messages',addr)    
+        
+        if mboxID[str(boxid)]:
+            send_msg = mboxID[str(boxid)].popleft()
+            s.sendto(send_msg.encode(),addr)
+
+        else:
+            s.sendto(b'No messages',addr)    
+    
+    elif command == 'quit':
+        s.close()
+        break
--- Practice/note2.py
+++ Practice/note2.py
@@ -110,3 +110,172 @@
 # print(result.decode())
 
 # sock.close()
+
+# import os
+# import socket
+
+# s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# port = 9999
+# s.bind(('',port))
+# s.listen(1)
+
+# while True:
+#     cli, addr = s.accept()
+#     print(addr,' connected')
+    
+#     #recv hello
+#     msg = cli.recv(1024)
+
+#     if not msg:
+#         cli.close()
+#         continue
+#     elif msg != b'Hello':
+#         cli.close()
+#         print('clinet: ',addr,msg.decode())
+#         continue
+#     else:
+#         print('client: ',addr,msg.decode())
+
+#     cli.send(b'Filename')
+
+#     bfilenname = cli.recv(1024)
+#     if not bfilenname:
+#         cli.close()
+#         continue
+
+#     filename = bfilenname.decode()
+#     print('client:',filename,'request.')
+
+#     try:
+#         filesize = os.path.getsize(filename)
+#     except:
+#         cli.send(b'nofile')
+#         cli.close()
+#         continue
+#     else:
+#         cli.send(str(filesize).encode())
+    
+#     f = open(filename,'rb')
+#     data = f.read()
+#     cli.sendall(data)
+
+#     msg = cli.recv(1024)
+#     if msg == b'bye':
+#         print('client',msg.decode())
+#         cli.close()
+#         f.close()
+
+# import os
+# import socket
+# import time
+
+# s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# s.bind(('',9999))
+# s.listen(1)
+
+# cli,addr = s.accept()
+# while True:
+#     print('Client Connected')
+#     cli.send(b'Request')
+
+#     data = cli.recv(1024)
+#     print(data.decode())
+
+#     temp,humid,light = data.decode().split()
+#     current_time = time.strftime('%c', time.localtime(time.time()))
+#     string = current_time + ':' + ' Temp:' + temp + ' Humid:' + humid + ' lilum:'+light +'\n'
+
+#     f = open('data','a')
+#     f.write(string)
+#     f.close()
+
+# import socket
+
+# sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+# while True:
+#     msg = input('Enter a message:')
+#     if msg == 'q':
+#         break
+#     sock.sendto(msg.encode(),('localhost',9999))
+#     data,addr = sock.recvfrom(1024)
+#     print("server says: ",data.decode())
+# sock.close()
+
+# import socket
+# sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+# while True:
+#     send = input('Send: ')
+#     sock.sendto(send.encode(),('localhost',3000))
+#     if send == 'q':
+#         sock.close()
+#     data,addr = sock.recvfrom(1024)
+#     print("Received: ",data.decode())
+
+# import socket
+
+# s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+# for i in range(10):
+#     time = 0.1
+#     data = 'hi iot'
+#     while True:
+#         s.sendto(data.encode(),('localhost',4000))
+#         print('packet({}) : Waiting up to {} secs for ack'.format(i,time))
+#         s.settimeout(time)
+#         try:
+#             data,addr = s.recvfrom(1024)
+#         except:
+#             time *= 2
+#             if time >2.0:
+#                 break
+#         else:
+#             print('Response',data.decode(),i)
+#             break
+
+# import socket
+# port = 3000
+
+# sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+# while True:
+#     msg = input('Enter the message("send mboxID message" or "receive mboxID"): ')
+
+#     if msg.startswith('send'): 
+#         sock.sendto(msg.encode(), ('localhost', port))
+#         print('OK')
+    
+#     elif msg.startswith('receive'):
+#         sock.sendto(msg.encode(), ('localhost', port))
+#         data, addr = sock.recvfrom(1024)
+#         print(data.decode())
+    
+#     elif msg == 'quit':
+#         sock.sendto('quit'.encode(), ('localhost', port))
+#         break
+
+# sock.close()
+
+import socket
+
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+while True:
+    command = input('Enter the message("send mboxID message" or "receive mboxID"): ')
+
+    if command.startswith('send'):
+        s.sendto(command.encode(),('localhost',3000))
+        print('OK')
+
+    elif command.startswith('receive'):
+        s.sendto(command.encode(),('localhost',3000))
+        msg , addr = s.recvfrom(1024)
+        print(msg.decode())
+
+    elif command.startswith('quit'):
+        s.sendto(b'quit',('localhost',3000))
+        s.close()
+        break
+
+    else:
+        continue
